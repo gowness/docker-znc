2ef7d172ef4c7bddcc9afb62c134111769feca6f
Gowness <biga05@gmail.com>
2015-03-10 20:44:47 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,7 +12,7 @@ apt-get update
 apt-get install python-software-properties
 add-apt-repository ppa:ubuntu-toolchain-r/test
 apt-get update
-apt-get install gcc-4.8
+apt-get install -y gcc-4.8
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
 
 # Install runtime dependencies.
