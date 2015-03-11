7f624ed4e532fa9755ebb1fac978112efc5c672e
Gowness <biga05@gmail.com>
2015-03-11 00:57:38 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,10 +10,11 @@ ZNC_VERSION="1.6.0"
 apt-get update
 
 apt-get install -y python-software-properties
-add-apt-repository ppa:ubuntu-toolchain-r/test
+add-apt-repository -y ppa:ubuntu-toolchain-r/test
 apt-get update
 apt-get install -y gcc-4.8 g++-4.8
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
+update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
 
 # Install runtime dependencies.
 apt-get install -y sudo
