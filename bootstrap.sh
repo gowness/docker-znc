77ec34beee8610da4ccdc72c548a33a8a624f0a7
Gowness <biga05@gmail.com>
2015-03-10 20:30:45 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,12 @@ ZNC_VERSION="1.6.0"
 # Ensure package list is up to date.
 apt-get update
 
+apt-get install python-software-properties
+add-apt-repository ppa:ubuntu-toolchain-r/test
+apt-get update
+apt-get install gcc-4.8
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
+
 # Install runtime dependencies.
 apt-get install -y sudo
 
