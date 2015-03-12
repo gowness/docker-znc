2ed2e4c87d98ae3ba6e37fa5dde3eea2e3653ca0
Gowness <biga05@gmail.com>
2015-03-11 22:44:10 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,19 +9,17 @@ ZNC_VERSION="1.6.0"
 # Ensure package list is up to date.
 apt-get update
 
-apt-get install -y python-software-properties
+# Install runtime dependencies.
+apt-get install -y sudo
+
+# Install build dependencies.
+apt-get install -y wget build-essential libssl-dev libperl-dev pkg-config python-software-properties
+
 add-apt-repository -y ppa:ubuntu-toolchain-r/test
 apt-get update
 apt-get install -y gcc-4.8 g++-4.8
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
 update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
-
-# Install runtime dependencies.
-apt-get install -y sudo
-
-# Install build dependencies.
-apt-get install -y wget build-essential libssl-dev libperl-dev pkg-config
-
 
 # Prepare building
 mkdir -p /src
