81a04dcad83041ba321505e888c87f482b4ca810
CoreOS Admin <core@herp.xyz>
2015-08-22 18:51:16 +0000

    converted to alpine linux

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,13 @@
-# version 1.4-1
-# docker-version 0.11.1
-FROM        ubuntu:12.04
+FROM        alpine
 MAINTAINER  gowness "biga05@gmail.com"
 
-# We use a bootstrap script to avoid having temporary cache files and build
-# dependencies being committed and included into the docker image.
+RUN apk update && apk upgrade && apk add autoconf automake gettext-dev g++ make openssl-dev pkgconfig zlib-dev git gdb
+
+
 ADD         bootstrap.sh /tmp/
 RUN         chmod +x /tmp/bootstrap.sh && /tmp/bootstrap.sh
 
-RUN         useradd znc
+RUN         adduser -D znc
 ADD         start-znc /usr/local/bin/
 ADD         znc.conf.default /src/
 RUN         chmod 644 /src/znc.conf.default
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,39 +1,21 @@
-#! /usr/bin/env bash
+#! /usr/bin/env ash
 set -e
 
-
-# Config
-ZNC_VERSION="1.6.0"
-
-
-# Ensure package list is up to date.
-apt-get update
-
-# Install runtime dependencies.
-apt-get install -y sudo
-
-# Install build dependencies.
-apt-get install -y wget build-essential libssl-dev libperl-dev pkg-config python-software-properties
-
-add-apt-repository -y ppa:ubuntu-toolchain-r/test
-apt-get update
-apt-get install -y gcc-4.8 g++-4.8
-update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
-update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
+#apk update 
+#apk upgrade   
+#apk add autoconf automake gettext-dev g++ make openssl-dev pkgconfig zlib-dev git gdb
 
 # Prepare building
 mkdir -p /src
 
-
 # Download, compile and install ZNC.
 cd /src
-wget "http://znc.in/releases/archive/znc-${ZNC_VERSION}.tar.gz"
-tar -zxf "znc-${ZNC_VERSION}.tar.gz"
-cd "znc-${ZNC_VERSION}"
+git clone https://github.com/znc/znc.git --recursive
+cd "znc"
+./bootstrap.sh 
 ./configure && make && make install
 
 
-# Clean up
-apt-get remove -y wget
-apt-get autoremove -y
-apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+#clean up 
+apk del  autoconf automake g++ make pkgconfig 
+rm  -rf /src/znc /var/cache/apk/*
